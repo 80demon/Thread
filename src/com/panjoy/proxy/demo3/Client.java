5bfb85bd8017d0e93f2b080bfa5f37ad873f422b
pengjin <pengjin@wumart.com>
2017-12-27 19:06:52 +0800

    11

--- src/com/panjoy/proxy/demo3/Client.java
+++ src/com/panjoy/proxy/demo3/Client.java
@@ -2,11 +2,10 @@ package com.panjoy.proxy.demo3;
 
 import com.panjoy.proxy.demo1.Target;
 import com.panjoy.proxy.demo1.TargetImpl;
-import com.panjoy.proxy.demo2.LogInterceptor;
 
 public class Client {
     public static void main(String[] args) {
-
+        //https://www.cnblogs.com/kakaxisir/p/4579110.html
         Interceptor interceptor=new LogInteceptor();
         Target target = (Target) interceptor.register(new TargetImpl());
         target.execute();
